7b20c99325ab23fc1d1e249a1ce49fb855550f70
Bogdan Yuskevich <yuskevich12@gmail.com>
2023-04-25 19:30:02 +0300

    Add implementation for task theOffice

---
+++ EasyLvlKatasTests/SolutionTest.java
@@ -0,0 +1,62 @@
+package EasyLvlKatasTests;
+
+import EasyLvlKatas.Person;
+import EasyLvlKatas.theOffice;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void basic1() {
+        Person[] meet = new Person[] {
+                new Person("tim", 0),
+                new Person("jim", 2),
+                new Person("randy", 0),
+                new Person("sandy", 7),
+                new Person("andy", 0),
+                new Person("katie", 5),
+                new Person("laura", 1),
+                new Person("saajid", 2),
+                new Person("alex", 3),
+                new Person("john", 2),
+                new Person("mr", 0)
+        };
+        assertEquals("Get Out Now!", theOffice.outed(meet, "laura"));
+    }
+
+    @Test
+    public void basic2() {
+        Person[] meet = new Person[] {
+                new Person("tim", 1),
+                new Person("jim", 3),
+                new Person("randy", 9),
+                new Person("sandy", 6),
+                new Person("andy", 7),
+                new Person("katie", 6),
+                new Person("laura", 9),
+                new Person("saajid", 9),
+                new Person("alex", 9),
+                new Person("john", 9),
+                new Person("mr", 8)
+        };
+        assertEquals("Nice Work Champ!", theOffice.outed(meet, "katie"));
+    }
+
+    @Test
+    public void basic3() {
+        Person[] meet = new Person[] {
+                new Person("tim", 2),
+                new Person("jim", 4),
+                new Person("randy", 0),
+                new Person("sandy", 5),
+                new Person("andy", 8),
+                new Person("katie", 6),
+                new Person("laura", 2),
+                new Person("saajid", 2),
+                new Person("alex", 3),
+                new Person("john", 2),
+                new Person("mr", 8),
+        };
+        assertEquals("Get Out Now!", theOffice.outed(meet, "john"));
+    }
+}
